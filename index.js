663d381b2138c67a5d4dab60a6a3b833e495f445
Pjotrs Baskakovs <baskakovs@me.com>
2022-06-26 13:55:03 +0300

    commit tree 590c132ee9d75d70d6e46364c0021f36380cf401

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(object) {
+    for (const user of object) {
+      if (user['result'] === "W") {
+        return (user['year']);
+      }
+    }
+  }
